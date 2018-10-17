a2f2d459f34274eeb7be457ea137e609ed6f6ea2
moses.adebayo <moses.adebayo@venturegardengroup.com>
2018-10-17 15:58:51 +0100

    ...

--- server.js
+++ server.js
@@ -2,13 +2,13 @@ const express = require('express')
 const next = require('next')
 
 const dev = process.env.NODE_ENV !== 'production'
+const port = process.env.PORT || '3000';
 const app = next({ dev })
 const handle = app.getRequestHandler()
 
 app.prepare()
 .then(() => {
-  const server = express()
-
+  const server = express();
   server.get('/p/:id', (req, res) => {
     const actualPage = '/post';
     const queryParams = { id: req.params.id } ;
@@ -19,9 +19,9 @@ app.prepare()
     return handle(req, res)
   })
 
-  server.listen(3000, (err) => {
-    if (err) throw err
-    console.log('> Ready on http://localhost:3000')
+  server.listen(`${port}`, (err) => {
+    if (err) throw err;
+    console.log(`> Ready on http://localhost:${port}`)
   })
 })
 .catch((ex) => {
